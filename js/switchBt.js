d25406d654a7a52fd6c20ee74dd6fb37324264ce
Crgio <sergioyalux1@gmail.com>
2020-11-11 17:34:18 -0300

    morse2Text feature added.

--- js/switchBt.js
+++ js/switchBt.js
@@ -1,11 +1,18 @@
-const btSwitch = document.getElementById("btSwitch");
-const btSwitchInt = document.getElementById("btSwitchInt");
-
-var init = 1;
+// const btSwitch = document.getElementById("btSwitch");
+// const btSwitchInt = document.getElementById("btSwitchInt");
 // initial state = 1
 // css applied state = 0
+var state1;
+var state2;
 
-const applyCss = (state) => {
+const init = () => {
+    state1 = 1;
+    state2 = 1;
+}
+
+const cssStyle = (state, idBtExt, idBtInt) => {
+    let btSwitch = document.getElementById(idBtExt);
+    let btSwitchInt = document.getElementById(idBtInt);
     if(state){
         btSwitch.style.background = "#fff";
         btSwitch.style.transition = "1s";
@@ -29,13 +36,53 @@ const applyCss = (state) => {
         btSwitchInt.style.transition = "1s";
         btSwitchInt.style.width = "30%";
     }
-    state = !state;
-    return state;
 }
 
-btSwitch.addEventListener("click",()=>{
-    init = applyCss(init);
-    // console.log(init);
-})
+const blockBT = (state) => {
+    let block = document.getElementById("block");
+    if(state){
+        block.style.backgroundColor = "#FF0000";
+        block.style.zIndex = 1;
+    }
+    else{
+        block.style.backgroundColor = "#FFFFFF";
+        block.style.zIndex = -1;
+    }
+}
+const changeTitle = (state) => {
+    let titlePt1 = document.getElementById("titlePt1");
+    let titlePt2 = document.getElementById("titlePt2");
+
+    if(state){
+        titlePt1.innerHTML = "morse2";
+        titlePt2.innerHTML = "Text";
+    }
+    else{
+        titlePt1.innerHTML = "text2";
+        titlePt2.innerHTML = "Morse";
+    }
+}
+
+const applyCss = (idBtExt, idBtInt) => {
+    let state;
+    if(idBtExt == "btSwitch"){
+        state = state1;
+        cssStyle(state,idBtExt,idBtInt);
+        state1 = !state1;
+    }
+    else{
+        state = state2;
+        blockBT(state);
+        changeTitle(state);
+        cssStyle(state,idBtExt,idBtInt);
+        state2 = !state2;
+    }
+    console.log(state);
+}
+
+// btSwitch.addEventListener("click",()=>{
+//     init = applyCss(init);
+//     // console.log(init);
+// })
 
 
